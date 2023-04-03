493a80e9a8617200b99c8dd738a40ac945910402
goldengrape <github@goldengrape.org>
2023-04-03 12:56:10 -0700

    增加字幕生成

--- text_to_speach.py
+++ text_to_speach.py
@@ -2,11 +2,27 @@ import edge_tts
 import asyncio
 import streamlit as st
 from utils import LANGUAGES_dict
+import os
 
 def main():
-    async def tts(text, VOICE) -> None:
+    async def tts(text, VOICE,OUTPUT_FILE,WEBVTT_FILE) -> None:
         communicate = edge_tts.Communicate(text, VOICE)
-        await communicate.save(OUTPUT_FILE)
+        submaker= edge_tts.SubMaker()
+        # await communicate.save(OUTPUT_FILE)
+        with open(OUTPUT_FILE, "wb") as file:
+            async for chunk in communicate.stream():
+                # print(chunk['type'])
+                if chunk['type']=='audio':
+                    file.write(chunk['data'])
+                elif chunk['type']=='WordBoundary':
+                    submaker.create_sub(
+                        (chunk['offset'], chunk['duration']),
+                        chunk['text']
+                    )
+        with open(WEBVTT_FILE, "w",encoding="utf-8") as file:
+            file.write(submaker.generate_subs())
+            print("WebVTT file saved to", WEBVTT_FILE)
+
     async def voice_selector(Language='en'):
         voices = await edge_tts.VoicesManager.create()
         voice = voices.find(Language=Language)
@@ -34,14 +50,25 @@ def main():
     if text_file is not None:
         text=text_file.read().decode("utf-8")
         OUTPUT_FILE = text_file.name.split(".")[0]+".mp3"
+        WEBVTT_FILE = text_file.name.split(".")[0]+".vtt"
     if convert_button:
         with st.spinner("Converting to Speech..."):
-            asyncio.run(tts(text, VOICE))
-        download_button=st.download_button(
+            asyncio.run(tts(text, VOICE,OUTPUT_FILE,WEBVTT_FILE))
+        download_audio_button=st.download_button(
             label="Download Audio",
             data=open(OUTPUT_FILE, "rb").read(),
             file_name=OUTPUT_FILE,
         )
-
+        download_sub_button=st.download_button(
+            label="Download Subtitles",
+            data=open(WEBVTT_FILE, "r").read(),
+            file_name=WEBVTT_FILE,
+        )
+        if download_audio_button:
+            # 清除临时文件
+            os.remove(OUTPUT_FILE)
+        if download_sub_button:
+            # 清除临时文件
+            os.remove(WEBVTT_FILE)
 if __name__ == "__main__":
     main()
