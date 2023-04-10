e3f003ae02402099a95efd7068b366130fc7340c
goldengrape <github@goldengrape.org>
2023-04-09 18:49:38 -0700

    test highlight

--- utils.py
+++ utils.py
@@ -4,9 +4,19 @@ import re
 import numpy as np
 
 
-sleep_time=60 
+def time_threshold():
+    t = 60
+    tmin=5
+    tmax=120
+    while True:
+        value = yield t
+        if value:
+            t = max(t/2,tmin)
+        else:
+            t = min(t+10,tmax)
+sleep_time=time_threshold()
+next(sleep_time)
 def query_gpt(prompt,cooldown_time=3):
-    global sleep_time
     while True:
         try:
             # start_time=time.time()
@@ -19,19 +29,17 @@ def query_gpt(prompt,cooldown_time=3):
             # print(f"GPT-3 API time: {time.time()-start_time}")
             answer=response.choices[0].message.content.strip()
             time.sleep(cooldown_time)
-            # print(f"after sleep 3s, I finished")
-            sleep_time = int(sleep_time/2)
-            sleep_time = max(sleep_time, 10)
+            sleep_time.send(True)
             break
         except:
             print(f"API error, retrying in {sleep_time} seconds...")
             time.sleep(sleep_time)
-            sleep_time += 10
-            if sleep_time > 120:
+            sleep_time.send(False)
+            if sleep_time == 120:
                 print("API error, aborting...")
                 answer=""
                 break
-    print(f"Answer: {answer}")
+    # print(f"Answer: {answer}")
     return answer
 
 LANGUAGES_dict = {
